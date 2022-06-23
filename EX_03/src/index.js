51279757c53b1ddd98d2a5bff2aa123916b89c22
jason810496 <810496@email.wlsh.tyc.edu.tw>
2022-06-23 14:21:07 +0800

    basic React template

--- EX_03/src/index.js
+++ EX_03/src/index.js
@@ -1,14 +1,57 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const JSX = (<h1>This is a JSX element!</h1>);
-const rootElement = document.getElementById( 'root' );
+const bgc = '#EDF6F9'
+
+const header_style = {
+    backgroundColor: bgc,
+    frontFamily: 'Helvetica Neue',
+    padding: '5px',
+}
 
 const header = (
-    <header>
-        <h1>Welcome to 30 Days Of React</h1>
-        <h2>Header 2 </h2>
+    <header style={ header_style }>
+        <h1>Learning React in 30 Days</h1>
+        <h2>Write html and CSS in JS</h2>
+        <h3>coool stuff </h3>
+        <small>June 23,2022</small>
     </header>
 );
 
-ReactDOM.render( JSX , rootElement );
+const main_style = {
+
+}
+
+const main = (
+    <main style={ main_style }>
+        <p>Preprocess : </p>
+        <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+        </ul>
+    </main>
+);
+
+const footer_style = {
+    backgroundColor: bgc
+}
+
+const footer = (
+    <footer style={ footer_style }>
+        <p> footer </p>
+    </footer>
+);
+
+const app = (
+    <div className='app'>
+        {header}
+        {main}
+        {footer}
+    </div>
+);
+
+
+
+const Root = document.getElementById( 'root' );
+ReactDOM.render( app , Root );
