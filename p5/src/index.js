96b1b1edb2b5c2ff97603ba5d41fb194f36bed54
jason810496 <810496@email.wlsh.tyc.edu.tw>
2022-06-29 10:45:13 +0800

    p5 init

--- p5/src/index.js
+++ p5/src/index.js
@@ -5,7 +5,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Sketch from 'react-p5';
-import './styles.css';
+// import './styles.css';
 
 
 class App extends React.Component {
